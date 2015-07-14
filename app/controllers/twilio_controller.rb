5b659d433f02a25ba06fefdae18593172ca85c29
Dave <dave.latham@gmail.com>
2015-07-13 22:49:50 -0700

    responding with temp to every sms

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,4 +1,5 @@
 class TwilioController < ApplicationController
+  require 'twilio-ruby'
   require 'nokogiri'
   require 'open-uri'
 
@@ -8,5 +9,14 @@ class TwilioController < ApplicationController
                               http_basic_authentication: [ENV['ISY_USER'], ENV['ISY_PASSWORD']]))
     # I HARDCODED THE ADDRESSES HERE, PROBABLY NOT THE BEST IDEA
     @thefile = @doc.xpath("nodes/node[@id='14 F7 28 1']/property[@id='ST']/@formatted | nodes/node[@id='23 40 20 1']/property[@id='ST']/@formatted")
+    return "it's currently #{@thefile[0].value.to_f.round(0)} in the living room and #{@thefile[1].value.to_f.round(0)} in the master bath"
+  end
+
+  def sms
+    @message = temperature
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message @message
+    end
+    render text: twiml.text
   end
 end
