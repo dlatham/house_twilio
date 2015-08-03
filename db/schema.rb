279a3a84b2d3f8ed2b7d79fbd4c1b9282ab4c324
Dave <dave.latham@gmail.com>
2015-08-03 15:56:01 -0700

    Updating to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150711204209) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "greetings", force: :cascade do |t|
     t.string   "text"
     t.boolean  "stranger"
