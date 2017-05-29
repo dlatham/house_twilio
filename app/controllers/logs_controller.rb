d5ab6aa9f13287037edc036702e4adf6e12e51c8
Dave Latham <dave.latham@gmail.com>
2017-05-29 15:49:03 -0700

    Added user to the logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -4,7 +4,7 @@ class LogsController < ApplicationController
   before_filter :authorize
 
   def index
-    @logs = Log.order("created_at DESC").paginate(:page => params[:page], :per_page => 20)
+    @logs = Log.select("logs.*, users.fname").joins("LEFT JOIN users ON logs.phone = users.phone").order("created_at DESC").paginate(:page => params[:page], :per_page => 20)
   end
 
 end
